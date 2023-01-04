e242637bb5ea2fcad9fe89020d333f39f3ecef59
Serkan Berat <serkanergovan57@gmail.com>
2023-01-04 20:32:58 +0300

    commit

---
+++ src/test/java/tr/edu/medipol/yoa/metin/MetinIslemleriTest.java
@@ -0,0 +1,25 @@
+package tr.edu.medipol.yoa.metin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MetinIslemleriTest {
+
+	@Test
+	public void testBosluklariTemizle() {
+			
+		//GIVEN * On Hazirliklar
+		String orjinalMetin ="Uc tas hos ho    saf . . .";
+		
+		
+		//WHEN	* Asil Testi gerceklestir
+		
+		String sonuc = MetinIslemleri.bosluklariTemizle(orjinalMetin);
+		
+		//THEN * Kontroller
+		
+		assertEquals("Uctashoshosaf...",sonuc);
+	}
+
+}
